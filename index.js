3e2f143ef3aba4aff9a5fa1673b12a4521d91a6f
Nikhil <nikhilashodariya123@gmail.com>
2020-01-27 16:41:35 -0500

    making changes

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ app.get("/", (req, res) => {
    res.send("This is great");
 });
 
+app.get("/nicks", (req, res) => {
+    console.log("in nicks");
+    res.send("this is nicks website");
+});
+
 app.listen(PORT, () => {
     console.log("server started at port ", PORT);
 });
